482dca242db7fbc36e1428ca330fde59e8fe6e0a
TaeHyong Kim <kthstar27@gmail.com>
2023-07-28 17:13:57 +0900

    2.0

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "ID"{
+    value = aws_instance.INS[*].id
+}
+
+output "PRIMARY_NIC_ID" {
+    value = try(aws_instance.INS[*].primary_network_interface_id, null)
+}
+
+output "DEFAULT_NIC_ID" {
+    value = try(aws_network_interface.DEFAULT_NIC[*].id, null)
+}
+
+output "PUBLIC_IP" {
+    value = try(aws_instance.INS[*].public_ip, null)
+}
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "PROFILE" {
+    type = string
+    default = null
+}
+
+variable "INSs" {
+    type = list(object({
+        NAME = string
+        KEY_NAME = string
+        AMI = string
+        TYPE = string
+        AUTO_PUBLIC_IP = optional(bool)
+        SRC_DEST_CHECK = optional(bool)
+        SN_ID = optional(string)
+        SG_IDs = optional(list(string))
+        PRI_IPV4s = optional(list(string))
+        VOL_DIR = string
+        VOL_SIZE = number
+        VOL_TYPE = string
+    }))
+    default = []
+}
+
+variable "INS_UDs" {
+    type = object({
+        FILE = list(list(string))
+        SCRIPT = list(string)
+    })
+
+    default = {
+        FILE = []
+        SCRIPT = []
+    }
+}
+
+# variable "INS_UD_FILEs" {
+#     type = list(list(string))
+#     default = []
+# }
+
+# variable "INS_UD_SCRIPTs" {
+#     type = list(string)
+#     default = []
+# }
